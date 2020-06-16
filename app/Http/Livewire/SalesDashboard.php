a97ec954b77d55a119e1b03942d1b13a6fbdea6b
dev-promtheus98 <ozh98@protonmail.com>
2020-06-16 22:41:37 +0000

    First commit

---
+++ app/Http/Livewire/SalesDashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SalesDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.sales-dashboard');
+    }
+}
